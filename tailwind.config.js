ff1455cb41fc6785acca310b693c7568a54a917d
DevPool <fakernguyen5831@gmail.com>
2025-03-21 16:34:13 +0700

    cm

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          customBlue: '#1E40AF',
+          customGreen: '#10B981',
+          customRed: '#EF4444',
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  };
